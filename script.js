e828e25f3855e8b26c5a920a27fd34863ee48d38
aliiiix <doraemonnobitatoons@gmail.com>
2026-09-06 22:39:56 +0530

    Fix fetch URL syntax in script.js

--- script.js
+++ script.js
@@ -347,7 +347,7 @@ orderBtn.addEventListener("click", async function () {
     };
 
     try {
-        const response = await fetch(https://printshop-q8id.onrender.com/api/orders, {
+        const response = await fetch("https://printshop-q8id.onrender.com/api/orders", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"

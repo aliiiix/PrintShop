d2e449902794022cef4d8dd6f315fee134b81e0b
aliiiix <doraemonnobitatoons@gmail.com>
2026-09-06 23:11:07 +0530

    Implement CORS middleware in server.js
    Added CORS middleware to handle preflight requests and set headers.

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
